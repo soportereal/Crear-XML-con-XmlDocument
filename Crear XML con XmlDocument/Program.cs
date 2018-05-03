9acbbaff7e2f2fcca0dc74084a3ca8b8705d2f79
soportereal <info@soportereal.com>
2018-05-03 13:49:23 -0600

    Initial commit

---
+++ Crear XML con XmlDocument/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Crear_XML_con_XmlDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode rootNode = xmlDoc.CreateElement("users");
+            xmlDoc.AppendChild(rootNode);
+
+            XmlNode userNode = xmlDoc.CreateElement("user");
+            XmlAttribute attribute = xmlDoc.CreateAttribute("age");
+            attribute.Value = "42";
+            userNode.Attributes.Append(attribute);
+
+            attribute = xmlDoc.CreateAttribute("sex");
+            attribute.Value = "male";
+            userNode.Attributes.Append(attribute);
+
+            userNode.InnerText = "John Doe";
+            rootNode.AppendChild(userNode);
+
+            userNode = xmlDoc.CreateElement("user");
+            attribute = xmlDoc.CreateAttribute("age");
+            attribute.Value = "39";
+            userNode.Attributes.Append(attribute);
+            userNode.InnerText = "Jane Doe";
+            rootNode.AppendChild(userNode);
+
+            xmlDoc.Save("test-doc.xml");
+        }
+    }
+}
